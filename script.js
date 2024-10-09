824cc9c24bb6c4bfcb4220569c03032451598b4c
Stephen <121040221+stephenstrange-blip@users.noreply.github.com>
2024-10-09 13:46:32 +0800

    build: Add hover effect to each grid

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log(maxGridWidth);
 for (let i = 1; i <= 16; i++){
 
     let gridRow = document.createElement("div");
-    let className = "gridRow" + i.toString();
+    let className = "gridRow"// + i.toString();
 
     gridRow.classList.add(className);
     //set width of row to full page width
@@ -26,15 +26,24 @@ for (let i = 1; i <= 16; i++){
     for (let j = 1; j <= 16; j++){
         
         let grid = document.createElement("div");
-        let className = "grid" + j.toString();
+        let className = "grid"// + j.toString();
 
         grid.classList.add(className);
         grid.setAttribute("style", `width: ${maxGridWidth}px; min-height: ${minHeight}px; border: 1px solid #000000; `)
         gridRow.appendChild(grid);
-    }
-    
-    
+    }  
 }
+
+const grids = document.querySelectorAll(".grid");
+grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () =>{
+        grid.style["background-color"] = "lightblue";
+    }) 
+    grid.addEventListener("mouseout", () => {
+        grid.style["background-color"] = "transparent";
+    })      
+})
+
 
  
 
